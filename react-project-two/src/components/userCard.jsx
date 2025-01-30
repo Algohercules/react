e9ce0a3825198069038f6c037f571802f0b675ae
algohercules <adarshrreso@gmail.com>
2025-01-31 00:34:16 +0530

    basic Bstructure providing

---
+++ react-project-two/src/components/userCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const userCard = () => {
+  return (
+    <div className='user-container'> 
+       <p>adarsh rai</p>
+       <img src="" alt="love"/>
+       <p>description of love babbar</p>
+    </div>
+  )
+}
+
+export default userCard
