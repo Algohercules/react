7431045f4f64ec88a6a9e456b06941c527e3fdfd
algohercules <adarshrreso@gmail.com>
2025-01-31 02:16:26 +0530

    learning syntaxes

--- react-project-two/src/components/userCard.jsx
+++ react-project-two/src/components/userCard.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import profilepic from '../assets/profilepic.jpg'
+import "./userCard.css"
+
+
+
 
 const UserCard = () => {
   return (
     <div className='user-container'  id='title'> 
        <p>adarsh rai</p>
-       <img src="" alt="love" id='user-image'/>
+       <img src={profilepic}  alt="love" id='user-image'/>
        <p id='user-description'>description of love babbar</p>
     </div>
   )
