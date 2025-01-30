428992518e15f8768e1e33911f752fd24b80ca4a
algohercules <adarshrreso@gmail.com>
2025-01-31 04:00:20 +0530

    created basic html lyout

---
+++ react-project-three/src/components/counter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const counter = () => {
+  return (
+    <div className='counter-container'>
+         <p id='para'>you have clicked 0 times</p>
+         <button id='button'>click me</button>
+    </div>
+  )
+}
+
+export default counter
