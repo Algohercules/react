27f7e990de244ff7e00a333f4debaa316ceb56ad
algohercules <adarshrreso@gmail.com>
2025-01-31 02:22:51 +0530

    a basic component created

--- react-project-two/src/App.jsx
+++ react-project-two/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <div className="container">
         <UserCard/>
         <UserCard/>        
-        <UserCard/>          
+        <UserCard/>        
       </div>
     </>
   )
