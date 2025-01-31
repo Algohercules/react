defcdc55686485b9b5b6e48a9756177ff38b6c98
algohercules <adarshrreso@gmail.com>
2025-01-31 10:29:20 +0530

    issue resolved we have to add import in jsx file of component

---
+++ react-project-2/src/components/UserCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const UserCard = () => {
+  return (
+    <div className='user-container'>
+        <p id='user-name'>adarsh rai</p>
+        <img  id='user-img' src="" alt="" />
+        <p id='user-desc'>description of adarsh</p>
+    </div>
+  )
+}
+
+export default UserCard
