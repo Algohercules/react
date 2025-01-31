78093b799138de4f58cf9cc14695fcf4b410681c
algohercules <adarshrreso@gmail.com>
2025-02-01 01:04:13 +0530

    props.children

--- react-project-4/src/App.jsx
+++ react-project-4/src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Card from './components/Card'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+   <div>
+       <Card name='love babbar'>
+        <h1> best web dev course</h1>
+        <p>trying to be consistent in this</p>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          will complete this course soon
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        </Card>
+   </div>
   )
 }
 
