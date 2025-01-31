f0bcacb6966ec90502f544cf3b9fa246bf904e4a
algohercules <adarshrreso@gmail.com>
2025-01-31 23:49:18 +0530

    imported counter

---
+++ react-project-3/src/components/Counter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Counter = () => {
+  return (
+    <div  className='counter-container'>
+        <p id="para">you have clicked me</p>
+        <button id="btn">click me</button>
+    </div>
+  )
+}
+
+export default Counter
