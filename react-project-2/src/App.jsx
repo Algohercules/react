655333125e3cbc0e210f1273c12225a0a3bb8f84
algohercules <adarshrreso@gmail.com>
2025-01-31 09:33:58 +0530

    restarting alll the repos

--- react-project-2/src/App.jsx
+++ react-project-2/src/App.jsx
@@ -1,18 +1,33 @@
-import UserCard from "./components/userCard"
-import userCard from "./components/userCard"
-
- import profilepic from '../assets/profilepic.jpg'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="container">
-        <UserCard name="adarsh rai "  image={profilepic} />
-        <UserCard name="maharana pratap"/>        
-        <UserCard  name= "prithviraj chauhan"/>        
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
