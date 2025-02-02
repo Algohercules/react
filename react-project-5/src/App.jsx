7bd25ed911b12e057d11df5a1229fb22261f5bbe
algohercules <adarshrreso@gmail.com>
2025-02-02 21:54:17 +0530

    styled the index.css

--- react-project-5/src/App.jsx
+++ react-project-5/src/App.jsx
@@ -1,12 +1,19 @@
+import { useState } from 'react'
 import './app.css'
 import Card from './components/Card'
 
 function App() {
  
+//create state
+//manage state
+//change state
+// sabhi child me state ko sync kardunga
+
+const [name,setName] = useState('');
 
   return (
     <div>
-      <Card name="adarsh rai"/>
+      <Card name={name}/ setName={setName}>
     </div>
   )
 }
