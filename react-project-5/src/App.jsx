993ebfab22d51d796cc51f7aae3dc158d77f9f8e
algohercules <adarshrreso@gmail.com>
2025-02-02 21:59:51 +0530

    change in child as well as parent componennt

--- react-project-5/src/App.jsx
+++ react-project-5/src/App.jsx
@@ -14,6 +14,7 @@ const [name,setName] = useState('');
   return (
     <div>
       <Card name={name} setName={setName}/>
+      <p>i am inside parent component and the value of component is {name}</p>
     </div>
   )
 }
