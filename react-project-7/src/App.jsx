e38b32f3cd5d70dac810b1ae53ea7ab37a8ad8e7
algohercules <adarshrreso@gmail.com>
2025-02-02 23:06:13 +0530

    para event listener crested

--- react-project-7/src/App.jsx
+++ react-project-7/src/App.jsx
@@ -5,12 +5,19 @@ function handleClick(){
         alert ("i am clicked")
 }
 
+function handleTouseOver(){
+  alert ("para ke upar mouse lekar aaye ho")
+}
 
   return (
     <div>
       <button onClick={handleClick}>
           click me  
       </button>
+
+      <p onMouseOver={handleTouseOver}>
+         this is the most prominent website you will ever be visiting
+      </p>
     </div>
   )
 }
