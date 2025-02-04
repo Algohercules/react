543235ee4e9faef6f90580d536853740e63324d9
algohercules <adarshrreso@gmail.com>
2025-02-04 15:16:26 +0530

    step 4 completed

--- react-project-9/src/components/ChildC.jsx
+++ react-project-9/src/components/ChildC.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import {userContext} from '../App'
 
 const ChildC = () => {
+    const user = userContext(userContext)
   return (
     <div>
-        hello ji
+        {user.name}
     </div>
   )
 }
